21fd13d1ac06d70bcad3c46009134ec9bde790a9
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2023-08-01 12:44:16 +0200

    task_#2205_TOOL_18 - removed Metadata

--- frontend/src/simpletools/T18/presentation/containers/T18Container.tsx
+++ frontend/src/simpletools/T18/presentation/containers/T18Container.tsx
@@ -3,148 +3,127 @@ import {IT18} from '../../types/T18.type';
 import {Background, Chart, Info, Parameters, Settings} from '../components';
 import SimpleToolGrid from 'components/SimpleToolGrid';
 import image from '../images/T18.png';
-// import uuidv4 from 'uuid';
 
 export const SETTINGS_INFILTRATION_TYPE_BASIN = 0.07;
 export const SETTINGS_INFILTRATION_TYPE_CYLINDER = 0.02;
 
 
 const defaults: IT18 = {
-  // FIXME npm i uuidv4 (id: uuidv4())
-  id: '123',
-  name: 'New simple tool',
-  description: 'Simple tool description',
-  permissions: 'rwx',
-  public: false,
-  tool: 'T18',
-  //FIXME  inputType, label, parseValue
-  data: {
-    settings: {
-      AF: 0.07,
-    },
-    parameters: [{
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 0,
-      id: 'LLRN',
-      name: 'Limiting loading rates<br/>N (kg/m²/y)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 500,
-      value: 67,
-      stepSize: 1,
-      type: 'int',
-      decimals: 0,
-    }, {
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 1,
-      id: 'LLRO',
-      name: 'Limiting loading rates<br/>BOD (kg/m²/y)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 1000,
-      value: 667,
-      stepSize: 1,
-      type: 'int',
-      decimals: 0,
-    }, {
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 2,
-      id: 'Q',
-      name: 'Flow rate<br/>Q (million m³/y)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 30,
-      value: 3.65,
-      stepSize: 0.01,
-      type: 'int',
-      decimals: 2,
-    }, {
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 3,
-      id: 'IR',
-      name: 'Infiltration rate<br/>I' + 'R'.sub() + '(m/y)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 1000,
-      value: 438,
-      stepSize: 1,
-      type: 'int',
-      decimals: 0,
-    }, {
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 4,
-      id: 'OD',
-      name: 'No. operation days per year<br/>OD (d)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 365,
-      validMax: x => 365 >= x,
-      value: 365,
-      stepSize: 1,
-      type: 'int',
-      decimals: 0,
-    }, {
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 5,
-      id: 'Cn',
-      name: 'Nitrogen concentration<br/>C' + 'N'.sub() + '(mg/l)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 100,
-      value: 40,
-      stepSize: 1,
-      type: 'int',
-      decimals: 0,
-    }, {
-      inputType: 'SLIDER',
-      label: '',
-      parseValue: parseFloat,
-      order: 6,
-      id: 'Co',
-      name: 'Organic concentration<br/>C' + 'O'.sub() + '(BOD in mg/l)',
-      min: 0,
-      validMin: x => 0 <= x,
-      max: 100,
-      value: 100,
-      stepSize: 1,
-      type: 'int',
-      decimals: 0,
-    }],
+  settings: {
+    AF: 0.07,
   },
+  parameters: [{
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 0,
+    id: 'LLRN',
+    name: 'Limiting loading rates<br/>N (kg/m²/y)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 500,
+    value: 67,
+    stepSize: 1,
+    type: 'int',
+    decimals: 0,
+  }, {
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 1,
+    id: 'LLRO',
+    name: 'Limiting loading rates<br/>BOD (kg/m²/y)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 1000,
+    value: 667,
+    stepSize: 1,
+    type: 'int',
+    decimals: 0,
+  }, {
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 2,
+    id: 'Q',
+    name: 'Flow rate<br/>Q (million m³/y)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 30,
+    value: 3.65,
+    stepSize: 0.01,
+    type: 'int',
+    decimals: 2,
+  }, {
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 3,
+    id: 'IR',
+    name: 'Infiltration rate<br/>I' + 'R'.sub() + '(m/y)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 1000,
+    value: 438,
+    stepSize: 1,
+    type: 'int',
+    decimals: 0,
+  }, {
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 4,
+    id: 'OD',
+    name: 'No. operation days per year<br/>OD (d)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 365,
+    validMax: x => 365 >= x,
+    value: 365,
+    stepSize: 1,
+    type: 'int',
+    decimals: 0,
+  }, {
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 5,
+    id: 'Cn',
+    name: 'Nitrogen concentration<br/>C' + 'N'.sub() + '(mg/l)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 100,
+    value: 40,
+    stepSize: 1,
+    type: 'int',
+    decimals: 0,
+  }, {
+    inputType: 'SLIDER',
+    label: '',
+    parseValue: parseFloat,
+    order: 6,
+    id: 'Co',
+    name: 'Organic concentration<br/>C' + 'O'.sub() + '(BOD in mg/l)',
+    min: 0,
+    validMin: x => 0 <= x,
+    max: 100,
+    value: 100,
+    stepSize: 1,
+    type: 'int',
+    decimals: 0,
+  }],
 };
 
 const T18 = () => {
   const [data, setData] = useState<IT18>(defaults);
-  const handleChangeSettings = (settings: IT18['data']['settings']) => {
-    setData((prevState) => ({
-      ...prevState,
-      data: {
-        ...prevState.data,
-        settings: {...settings},
-      },
-    }));
+  const handleChangeSettings = (settings: IT18['settings']) => {
+    setData((prevState) => ({...prevState, settings: {...settings}}));
+
   };
 
-  const handleChangeParameters = (parameters: IT18['data']['parameters']) => {
-    setData((prevState) => ({
-      ...prevState,
-      data: {
-        ...prevState.data,
-        parameters: [...parameters],
-      },
-    }));
+  const handleChangeParameters = (parameters: IT18['parameters']) => {
+    setData((prevState) => ({...prevState, parameters: [...parameters]}));
+
   };
   const handleReset = () => {
     setData(defaults);
@@ -154,15 +133,15 @@ const T18 = () => {
     <SimpleToolGrid rows={2}>
       <Background image={image} title={'T08: 1D transport equation (Ogata-Banks)'}/>
       <Chart
-        settings={data.data.settings}
-        parameters={data.data.parameters}
+        settings={data.settings}
+        parameters={data.parameters}
       />
       <div>
-        <Settings settings={data.data.settings} onChange={handleChangeSettings}/>
-        <Info parameters={data.data.parameters} settings={data.data.settings}/>
+        <Settings settings={data.settings} onChange={handleChangeSettings}/>
+        <Info parameters={data.parameters} settings={data.settings}/>
       </div>
       <Parameters
-        parameters={data.data.parameters}
+        parameters={data.parameters}
         onChange={handleChangeParameters}
         onReset={handleReset}
       />
--- frontend/src/simpletools/T18/types/T18.type.ts
+++ frontend/src/simpletools/T18/types/T18.type.ts
@@ -1,32 +1,24 @@
 type IInputType = 'SLIDER' | 'NUMBER' | 'RADIO_SELECT' | 'SELECT_NAME';
 
 export interface IT18 {
-  id: string;
-  name: string;
-  description: string;
-  permissions: string;
-  public: boolean;
-  tool: string;
-  data: {
-    parameters: {
-      order: number;
-      id: string;
-      name: string;
-      min: number;
-      max: number;
-      value: number;
-      stepSize: number;
-      type: string;
-      decimals: number;
-      inputType: IInputType;
-      label: string;
-      parseValue: (value: string) => number;
-      validMin?: (x: number) => boolean;
-      validMax?: (x: number) => boolean;
-    }[];
-    settings: {
-      AF: number;
-    };
+  parameters: {
+    order: number;
+    id: string;
+    name: string;
+    min: number;
+    max: number;
+    value: number;
+    stepSize: number;
+    type: string;
+    decimals: number;
+    inputType: IInputType;
+    label: string;
+    parseValue: (value: string) => number;
+    validMin?: (x: number) => boolean;
+    validMax?: (x: number) => boolean;
+  }[];
+  settings: {
+    AF: number;
   };
 }
 
