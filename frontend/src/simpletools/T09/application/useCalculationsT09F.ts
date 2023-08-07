de9651f8fd7039fffbd5541269e1d4f3a2714f1c
Ralf Junghanns <ralf@junghanns.it>
2023-08-07 16:27:13 +0200

    Bugfix Tool T09F

--- frontend/src/simpletools/T09/application/useCalculationsT09F.ts
+++ frontend/src/simpletools/T09/application/useCalculationsT09F.ts
@@ -1,20 +1,24 @@
-const calcAlpha = ({w, k, df, ds}: { w: number; k: number; df: number; ds: number }): number =>
-  Math.sqrt((w * (ds - df)) / (k * ds));
-
+const calcAlpha = ({w, k, df, ds}: { w: number; k: number; df: number; ds: number }): number => Math.sqrt((w * (ds - df)) / (k * ds));
 const calcBeta = ({df, ds}: { df: number; ds: number }): number => df / (ds - df);
 
-export function calcXt({k, z0, l, w, df, ds}: { k: number; z0: number; l: number; w: number; df: number; ds: number }): number {
+export const calcXt = ({k, z0, l, w, df, ds}: { k: number; z0: number; l: number; w: number; df: number; ds: number }): number => {
   const alpha = calcAlpha({w, k, df, ds});
   const beta = calcBeta({df, ds});
 
-  return Math.sqrt(Math.pow(l, 2) - Math.pow((z0 / (alpha * beta)), 2));
-}
+  const result = Math.sqrt(Math.pow(l, 2) - Math.pow((z0 / (alpha * beta)), 2));
 
-export function calcDeltaXt({dz, k, z0, l, w, theta, df, ds}: { dz: number; k: number; z0: number; l: number; w: number; theta: number; df: number; ds: number }): number {
+  if (isNaN(result)) {
+    return 0;
+  }
+
+  return result;
+};
+
+export const calcDeltaXt = ({dz, k, z0, l, w, theta, df, ds}: { dz: number; k: number; z0: number; l: number; w: number; theta: number; df: number; ds: number }): number => {
   const alpha = calcAlpha({w, k, df, ds});
   const beta = calcBeta({df, ds});
 
-  return (
+  const result = (
     Math.sqrt(
       Math.pow(l - (dz / Math.tan(theta * Math.PI / 180)), 2) -
       Math.pow((z0 + dz) / (alpha * beta), 2),
@@ -23,20 +27,26 @@ export function calcDeltaXt({dz, k, z0, l, w, theta, df, ds}: { dz: number; k: n
       Math.pow(l, 2) - Math.pow((z0 / (alpha * beta)), 2),
     )
   );
-}
 
-export function calcNewXt({dz, k, z0, l, w, theta, df, ds}: { dz: number; k: number; z0: number; l: number; w: number; theta: number; df: number; ds: number }): number {
+  if (isNaN(result)) {
+    return 0;
+  }
+
+  return result;
+};
+
+export const calcNewXt = ({dz, k, z0, l, w, theta, df, ds}: { dz: number; k: number; z0: number; l: number; w: number; theta: number; df: number; ds: number }): number => {
   const xt = calcXt({k, z0, l, w, df, ds});
   const deltaXt = calcDeltaXt({dz, k, z0, l, w, theta, df, ds});
   return xt + deltaXt;
-}
+};
 
-export function calcH({k, l, w, x, df, ds}: { k: number; l: number; w: number; x: number; df: number; ds: number }): number {
+export const calcH = ({k, l, w, x, df, ds}: { k: number; l: number; w: number; x: number; df: number; ds: number }): number => {
   const alpha = calcAlpha({w, k, df, ds});
   return alpha * Math.sqrt(Math.pow(l, 2) - Math.pow(x, 2));
-}
+};
 
-export function calcI({dz, k, l, w, theta, x, df, ds}: { dz: number; k: number; l: number; w: number; theta: number; x: number; df: number; ds: number }): number {
+export const calcI = ({dz, k, l, w, theta, x, df, ds}: { dz: number; k: number; l: number; w: number; theta: number; x: number; df: number; ds: number }): number => {
   const alpha = calcAlpha({w, k, df, ds});
   const h = calcH({k, l, w, x, df, ds});
   const tanTheta = Math.tan(theta * Math.PI / 180);
@@ -48,4 +58,4 @@ export function calcI({dz, k, l, w, theta, x, df, ds}: { dz: number; k: number; 
       Math.pow(h, 2),
     ) - h
   );
-}
+};
