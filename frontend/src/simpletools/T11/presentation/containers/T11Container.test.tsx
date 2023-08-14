a17b881b70539969a0dfb8080203411e32fe23ac
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2023-08-14 12:04:19 +0200

    task_2243_T11 (changed MemoryRouter to BrowserRouter)

--- frontend/src/simpletools/T11/presentation/containers/T11Container.test.tsx
+++ frontend/src/simpletools/T11/presentation/containers/T11Container.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import {MemoryRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import T11 from './T11Container';
 
 
 describe('T11Container Tests', () => {
   test('It renders the component', async () => {
     render(
-      <MemoryRouter>
+      <BrowserRouter>
         <T11/>
-      </MemoryRouter>,
+      </BrowserRouter>,
     );
 
     expect(screen.getByTestId('T11-container')).toBeInTheDocument();
