87865cd442a5e744935c1feddddacac179a30428
Ralf Junghanns <ralf@junghanns.it>
2023-08-15 10:47:56 +0200

    Update setupTests.ts

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,1 +1,4 @@
-window.URL.createObjectURL = function() {};
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'jest-canvas-mock';
+// @ts-ignore
+window.URL.createObjectURL = function () {};
