6e279789f616a53768d3455e5f57251ba2c41292
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2023-08-29 09:19:46 +0200

    task_2200_T08_Tests (added infrastructure T08)

--- frontend/src/simpletools/T08/application/useCalculateMounding.ts
+++ frontend/src/simpletools/T08/application/useCalculateMounding.ts
@@ -1,5 +1,6 @@
 import {SETTINGS_CASE_FIXED_TIME, SETTINGS_CASE_VARIABLE_TIME, SETTINGS_INFILTRATION_ONE_TIME} from '../presentation/containers/T08Container';
 import {IT08} from '../types/T08.type';
+import {calcC, calcCTau, calcT, calcX} from '../infrastructure';
 
 interface IUseCalculateMounding {
   calcC: (t: number, x: number, vx: number, R: number, DL: number) => number;
@@ -19,67 +20,6 @@ interface IUseCalculateMounding {
     tau: number
   ) => any[]; // Update this to match the actual return type
 }
-
-const erf = (x: number) => {
-  const a1: number = 0.254829592;
-  const a2: number = -0.284496736;
-  const a3: number = 1.421413741;
-  const a4: number = -1.453152027;
-  const a5: number = 1.061405429;
-  const p: number = 0.3275911;
-
-  // Save the sign of x
-  let sign = 1;
-  if (0 > x) {
-    sign = -1;
-  }
-  // get absX
-  const absX: number = Math.abs(x);
-
-  // A & S 7.1.26 with Horners Method
-  const t: number = 1.0 / (1.0 + p * absX);
-  const y: number = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.exp(-absX * absX);
-  return sign * y;
-};
-
-const erfc = (x: number) => {
-  return 1 - erf(x);
-};
-
-const calcC = (t: number, x: number, vx: number, R: number, DL: number): number => {
-  const term1: number = erfc((x - (vx * t / R)) / (2 * Math.sqrt(DL * t / R)));
-  const term2: number = erfc((x + (vx * t / R)) / (2 * Math.sqrt(DL * t / R)));
-
-  return 0.5 * (term1 + Math.exp(vx * x / DL) * term2);
-};
-
-const calcCTau = (t: number, x: number, vx: number, R: number, DL: number, tau: number): number => {
-  const term1 = erfc((x - (vx * t / R)) / (2 * Math.sqrt(DL * t / R))) - erfc((x - (vx * (t - tau) / R)) / (2 * Math.sqrt(DL * (t - tau) / R)));
-  let term2 = erfc((x + (vx * t / R)) / (2 * Math.sqrt(DL * t / R))) - erfc((x + (vx * (t - tau) / R)) / (2 * Math.sqrt(DL * (t - tau) / R)));
-  term2 = 10e-16 > Math.abs(term2) ? 0 : term2;
-  return 0.5 * (term1 + Math.exp(vx * x / DL) * term2);
-};
-
-const calcT = (xMax: number, vx: number, R: number, DL: number): number => {
-  let c = 0;
-  let t = 0;
-  while (0.9999 > c) {
-    c = calcC(t, xMax, vx, R, DL);
-    t = t + 20;
-  }
-
-  return t;
-};
-
-const calcX = (tMax: number, vx: number, R: number, DL: number): number => {
-  let c = 1;
-  let x = 0;
-  while (0.0001 < c) {
-    c = calcC(tMax, x, vx, R, DL);
-    x = x + 20;
-  }
-  return x;
-};
 
 const useCalculateMounding = (): IUseCalculateMounding => ({
   calcC: calcC,
---
+++ frontend/src/simpletools/T08/infrastructure/calcCTau.ts
@@ -0,0 +1,9 @@
+import {erfc} from './index';
+
+const calcCTau = (t: number, x: number, vx: number, R: number, DL: number, tau: number): number => {
+  const term1 = erfc((x - (vx * t / R)) / (2 * Math.sqrt(DL * t / R))) - erfc((x - (vx * (t - tau) / R)) / (2 * Math.sqrt(DL * (t - tau) / R)));
+  let term2 = erfc((x + (vx * t / R)) / (2 * Math.sqrt(DL * t / R))) - erfc((x + (vx * (t - tau) / R)) / (2 * Math.sqrt(DL * (t - tau) / R)));
+  term2 = 10e-16 > Math.abs(term2) ? 0 : term2;
+  return 0.5 * (term1 + Math.exp(vx * x / DL) * term2);
+};
+export default calcCTau;
