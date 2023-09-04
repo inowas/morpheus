2b9f0d0fa225b89c21ae574f5a390669f21fd070
Christian Rüdiger <christian.ruediger@itexia.com>
2023-09-04 14:09:23 +0200

    update openapi spec

--- backend/src/morpheus/authentication/infrastructure/oauth2/models.py
+++ backend/src/morpheus/authentication/infrastructure/oauth2/models.py
@@ -2,6 +2,16 @@ import time
 from authlib.integrations.sqla_oauth2 import OAuth2ClientMixin, OAuth2TokenMixin
 from morpheus.common.infrastructure.persistence.database import db
 
+class OAuth2User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.String(36), primary_key=True)
+    email = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def get_user_id(self) -> str:
+        return self.id
+
 
 class OAuth2Client(db.Model, OAuth2ClientMixin):
     __tablename__ = 'oauth2_client'
