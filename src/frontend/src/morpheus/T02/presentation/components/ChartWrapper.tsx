0e018b8602a3daab4326465afd1e763ba0e69b02
Ralf Junghanns <ralf@junghanns.it>
2023-10-16 22:39:14 +0200

    Move ChartModal to Components

--- src/frontend/src/morpheus/T02/presentation/components/ChartWrapper.tsx
+++ src/frontend/src/morpheus/T02/presentation/components/ChartWrapper.tsx
@@ -1,12 +1,7 @@
 import React, {useState} from 'react';
-import {Grid, Icon} from 'semantic-ui-react';
+import {ChartModal, Icon, Grid} from 'components';
 import Chart from './Chart';
 import styles from '../containers/T02Container.module.less';
-
-interface IMounding {
-  calculateHi: (x: number, y: number, w: number, L: number, W: number, hi: number, Sy: number, K: number, t: number) => number;
-  calculateHMax: (x: number, y: number, w: number, L: number, W: number, hi: number, Sy: number, K: number, t: number) => number;
-}
 
 interface IProps {
   data: { L: any, W: any, xData: number[], yData: number[], zData: any[][] };
@@ -15,31 +10,17 @@ interface IProps {
 const ChartWrapper = ({data}: IProps) => {
 
   const {L, W, xData, yData, zData} = data;
-
   const [showModal, setShowModal] = useState(false);
-
-  const handleToggleModal = () => {
-    setShowModal(!showModal);
-  };
-
-  // useEffect(() => {
-  //   console.log('parameters updated in ChartWrapper');
-  // }, [parameters]);
-  //
-  // useEffect(() => {
-  //   console.log('loading updated in ChartWrapper');
-  // }, [loading]);
-
 
   return (
     <>
-      <Grid style={{
+      <Grid.Grid style={{
         height: '100%',
       }}
       >
         <Icon
           name="expand arrows alternate"
-          onClick={handleToggleModal}
+          onClick={() => setShowModal(!showModal)}
           className={styles.icon}
           style={{position: 'absolute', zIndex: '2', cursor: 'pointer'}}
         />
@@ -61,22 +42,21 @@ const ChartWrapper = ({data}: IProps) => {
             id="chart1"
           />
         </Grid.Column>
-      </Grid>
-      {/*{showModal && (*/}
-      {/*  // <ChartModal open={true} onClose={handleToggleModal}>*/}
-      {/*  //   <Chart*/}
-      {/*  //     data={{*/}
-      {/*  //       x: xData,*/}
-      {/*  //       y: yData,*/}
-      {/*  //       z: zData,*/}
-      {/*  //     }}*/}
-      {/*  //     basinLength={L}*/}
-      {/*  //     basinWidth={W}*/}
-      {/*  //     id="chart2"*/}
-      {/*  //     onLoad={onLoad}*/}
-      {/*  //   />*/}
-      {/*  // </ChartModal>*/}
-      {/*)}*/}
+      </Grid.Grid>
+      {showModal && (
+        <ChartModal open={true} onClose={() => setShowModal(!showModal)}>
+          <Chart
+            data={{
+              x: xData,
+              y: yData,
+              z: zData,
+            }}
+            basinLength={L}
+            basinWidth={W}
+            id="chart2"
+          />
+        </ChartModal>
+      )}
     </>
   );
 };
