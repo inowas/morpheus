4f92c2646151318c3852be97421da30c0b09a344
Ralf Junghanns <ralf@junghanns.it>
2023-10-17 09:08:05 +0200

    Remove AuthProvider

--- src/frontend/src/morpheus/App.tsx
+++ src/frontend/src/morpheus/App.tsx
@@ -1,30 +1,15 @@
 import '../../styleguide/semantic.less';
 import './App.less';
-import {AuthProvider} from 'oidc-react';
 
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
 import Routes from './routes';
 
-const oidcConfig = {
-  onSignIn: (response: any) => {
-    alert('The user has signed in');
-    console.log({response});
-  },
-  authority: 'https://identity.inowas.localhost/realms/inowas/',
-  clientId: 'morpheus-frontend',
-  responseType: 'code',
-  redirectUri: 'http://localhost:4000',
-  scope: 'openid profile email',
-};
-
 const App = () => (
-  <AuthProvider {...oidcConfig}>
-    <BrowserRouter basename="/">
-      <Routes/>
-    </BrowserRouter>
-  </AuthProvider>
+  <BrowserRouter basename="/">
+    <Routes/>
+  </BrowserRouter>
 );
 
 export default App;
