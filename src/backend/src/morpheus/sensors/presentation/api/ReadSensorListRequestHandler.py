51eb2686e22b7813ac91c4fac985562149b5d7ff
Ralf Junghanns <ralf@junghanns.it>
2023-10-24 09:27:16 +0200

    Refactor Sensor Module Architecture

--- src/backend/src/morpheus/sensors/presentation/api/ReadSensorListRequestHandler.py
+++ src/backend/src/morpheus/sensors/presentation/api/ReadSensorListRequestHandler.py
@@ -1,17 +1,11 @@
-from flask import Request, abort
+from flask import Request
 
-from ...application.read.ReadSensorList import ReadSensorListQuery, \
-    ReadSensorListQueryHandler, ReadSensorListException
+from ...application.read.ReadSensorList import ReadSensorListQuery, ReadSensorListQueryHandler
 
 
 class ReadSensorListRequestHandler:
     @staticmethod
     def handle(request: Request):
         projects = ['BRA1', 'BRA2', 'DEU1', 'KAZ', 'LFF']
-        try:
-            result = ReadSensorListQueryHandler.handle(ReadSensorListQuery(projects=projects))
-            return result.to_dict(), 200
-        except ReadSensorListException as e:
-            return abort(500, str(e))
-        except Exception as e:
-            return abort(500, str(e))
+        result = ReadSensorListQueryHandler.handle(ReadSensorListQuery(projects=projects))
+        return result.to_dict(), 200
