19b65efa298e30afa98c8edc79e98a9839291112
Ralf Junghanns <ralf@junghanns.it>
2023-10-25 15:51:16 +0000

    Feature/2326 add cron container

--- infrastructure/local/mongodb_backend/mongo-init.js
+++ infrastructure/local/mongodb_backend/mongo-init.js
@@ -17,3 +17,23 @@ try {
   print(error);
   print('----------');
 }
+
+try {
+  print(`-----${__filename}-----`);
+  print(`Creating user ${process.env.BACKEND_MONGO_USER} for database ${process.env.BACKEND_MONGO_MODFLOW_DATABASE}`);
+  print('----------');
+  db = db.getSiblingDB(process.env.BACKEND_MONGO_MODFLOW_DATABASE);
+  db.createUser({
+    user: process.env.BACKEND_MONGO_USER,
+    pwd: process.env.BACKEND_MONGO_PASSWORD,
+    roles: [{
+      role: "readWrite",
+      db: process.env.BACKEND_MONGO_MODFLOW_DATABASE,
+    }],
+  });
+} catch (error) {
+  print(`-----${__filename}-----`);
+  print(`Could not create user ${process.env.BACKEND_MONGO_USER}.`);
+  print(error);
+  print('----------');
+}
