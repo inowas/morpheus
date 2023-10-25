58181f90ae1ad4356d60b652f3c9d2a0f3b93354
Ralf Junghanns <ralf@junghanns.it>
2023-10-25 14:29:22 +0200

    Resolve comments and merge main

--- infrastructure/production/morpheus/cron/jobs/hourly/sync_uit_sensors.sh
+++ infrastructure/production/morpheus/cron/jobs/hourly/sync_uit_sensors.sh
@@ -7,5 +7,5 @@ script_name=$(basename "$0")
 
 # run the script
 echo "Running ${script_name} at $(date)"
-docker exec -i -u flask morpheus_dev-backend flask sensors sync-uit-sensors
+docker exec -i "${BACKEND_CONTAINER_NAME}" flask sensors sync-uit-sensors
 echo "Finished ${script_name} at $(date)"
