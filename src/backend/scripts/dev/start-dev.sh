9d02f10aa3617c51c4bd846bad06c11f13964988
Christian Rüdiger <christian.ruediger@itexia.com>
2023-10-31 16:33:14 +0100

    add script to run dev flask app from command line

--- src/backend/scripts/dev/start-dev.sh
+++ src/backend/scripts/dev/start-dev.sh
@@ -3,6 +3,13 @@
 # include file with helper functions
 source "$(dirname "$0")/util.inc.sh"
 
+# if .env file already exists we assume that the dev environment is already running
+if [ -f "$backendEnvFile" ]; then
+  outputSuccess "Dev environment is already running"
+  echo "To restart dev environment you must first stop it"
+  exit 0
+fi
+
 docker compose -f "$localInfrastructureRoot/docker-compose.yml" \
   --profile mailcatcher \
   --profile keycloak \
@@ -16,3 +23,6 @@ set -a # automatically export all variables from .env file
 source $backendEnvFile
 set +a
 $backendRoot/docker/docker-entrypoint.sh
+exitWithErrorIfLastCommandFailed "Error running entrypoint script"
+
+outputSuccess "Dev environment started"
