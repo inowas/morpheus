c41f9a1f4cd4317993e61d059df4387060f76279
Christian Rüdiger <christian.ruediger@itexia.com>
2023-11-06 10:21:30 +0100

    use local npm for openapi spec build script

--- src/backend/scripts/dev/build-openapi-spec.sh
+++ src/backend/scripts/dev/build-openapi-spec.sh
@@ -2,7 +2,9 @@
 
 source "$(dirname "$0")/util.inc.sh"
 
-npm install -g @apidevtools/swagger-cli
+cd $backendRoot
+
+npm install @apidevtools/swagger-cli
 exitWithErrorIfLastCommandFailed "Error installing swagger-cli"
 
 swagger-cli bundle -o src/morpheus/openapi.bundle.yml -r -t yaml src/morpheus/openapi.yml
