1936e3424ce5de3ae68a05c5f35ba9e5d72190d8
Dmytro Pasichnyk <dmytro.pasichnyk@mailbox.tu-dresden.de>
2023-11-08 12:21:33 +0000

    T02_tool (delete setings)

--- src/frontend/src/morpheus/T02/presentation/components/index.ts
+++ src/frontend/src/morpheus/T02/presentation/components/index.ts
@@ -2,12 +2,10 @@ import Background from './Background';
 import ChartWrapper from './ChartWrapper';
 import Info from './Info';
 import Parameters from './Parameters';
-import Settings from './Settings';
 
 export {
   Background,
   ChartWrapper,
   Info,
   Parameters,
-  Settings,
 };
