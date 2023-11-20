a0c6651301cb01ebe0490b12467a9454dfe1e2c8
Ralf Junghanns <ralf@junghanns.it>
2023-11-20 11:24:51 +0000

    Feature/2343 add deployment of storybook to storybook.inowas.org

--- src/frontend/src/components/Charts/SurfacePlot3D.tsx
+++ src/frontend/src/components/Charts/SurfacePlot3D.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import {usePlotly} from 'common/hooks';
 
 interface IProps {
   title?: string;
@@ -15,26 +16,24 @@ interface ISurfacePlotData {
 type I2DArr = Array<Array<number>>;
 
 const SurfacePlot3D: React.FC<IProps> = ({data, title, style}) => {
+  const plotly = usePlotly();
 
   useEffect(() => {
-
-    if (!window.Plotly) {
+    if (!plotly) {
       throw Error('Plotly not found.');
     }
-    const Plotly = window.Plotly;
     const plotData = data.map((d) => ({...d, type: 'surface'}));
     const config = {responsive: true};
     const layout = {title, autosize: true, margin: {l: 0, r: 0, b: 0, t: 0}};
-    Plotly.newPlot('plotlyContainer', plotData, layout, config);
+    plotly.newPlot('plotlyContainer', plotData, layout, config);
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data, title]);
 
 
   return (
-    <div
-      id="plotlyContainer"
-      style={style || {width: '100%', height: '100%'}}
-    ></div>);
+    <div id="plotlyContainer" style={style || {width: '100%', height: '100%'}}></div>
+  );
 };
 
 export default SurfacePlot3D;
