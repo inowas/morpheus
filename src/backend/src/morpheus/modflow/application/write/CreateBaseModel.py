3e20210544553d9986fb4fe8a78fe878ee669579
Ralf Junghanns <ralf@junghanns.it>
2023-11-21 17:05:57 +0100

    Add notebook for linear interpolation

--- src/backend/src/morpheus/modflow/application/write/CreateBaseModel.py
+++ src/backend/src/morpheus/modflow/application/write/CreateBaseModel.py
@@ -1,12 +1,13 @@
 import dataclasses
 
-from ...infrastructure.geometry import utils
 from ...infrastructure.persistence.BaseModelRepository import BaseModelRepository
 from ...types.Project import ProjectId
 from ...types.User import UserId
 from ...types.ModflowModel import ModflowModel
-from morpheus.modflow.types.discretization.spatial.SpatialDiscretization import Polygon, CRS, SpatialDiscretization, \
-    CreateGridDict, LengthUnit, Rotation
+from ...types.discretization.spatial import GridCells
+from ...types.discretization.spatial.SpatialDiscretization import Polygon, SpatialDiscretization, Rotation
+from ...types.discretization.spatial.Crs import Crs
+from ...types.discretization.spatial.Grid import CreateGridDict, Grid
 
 
 @dataclasses.dataclass(frozen=True)
@@ -29,24 +30,18 @@ class CreateBaseModelCommandHandler:
         project_id = command.project_id
         base_model = ModflowModel.new()
 
-        grid = utils.grid_from_polygon(
+        grid = Grid.from_polygon_with_relative_coordinates(
             polygon=command.geometry,
             rotation=Rotation.from_float(command.grid_properties['rotation']),
-            length_unit=LengthUnit.from_str(command.grid_properties['length_unit']),
             x_coordinates=command.grid_properties['x_coordinates'],
             y_coordinates=command.grid_properties['y_coordinates'],
         )
-
-        affected_cells = utils.calculate_cells_from_polygon(
-            geometry=command.geometry,
-            grid=grid
-        )
-
+        cells = GridCells.from_polygon(polygon=command.geometry, grid=grid)
         spatial_discretization = SpatialDiscretization(
             geometry=command.geometry,
-            affected_cells=affected_cells,
+            affected_cells=cells,
             grid=grid,
-            crs=CRS.from_str('EPSG:4326')
+            crs=Crs.from_str('EPSG:4326')
         )
 
         base_model = base_model.with_updated_spatial_discretization(spatial_discretization)
