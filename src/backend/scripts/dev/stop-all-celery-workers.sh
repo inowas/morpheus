f96603d5c52df451a73391d2796dd4ad4f6f9024
Ralf Junghanns <ralf@junghanns.it>
2023-12-08 11:04:25 +0100

    Update Notebooks

---
+++ src/backend/scripts/dev/stop-all-celery-workers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# include file with helper functions
+source "$(dirname "$0")/util.inc.sh"
+
+$devScriptsRoot/start-dev.sh
+
+cd $backendRoot/src
+source $backendRoot/.venv/bin/activate &&  celery -A task_queue control shutdown
