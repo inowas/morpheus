f9cb8b109f0de68c47330171222409508bc0d79f
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2023-12-14 10:02:40 +0100

    Task_#2350 (Sidebar close button update)

--- src/frontend/src/components/Sidebar/Sidebar.tsx
+++ src/frontend/src/components/Sidebar/Sidebar.tsx
@@ -39,7 +39,7 @@ const Sidebar = ({children, data, updateModelData}: IProps) => {
         className={styles.sidebarStickyButton}
         onClick={handleClose}
       >
-        <Icon name="cogs"/>
+        <Icon name="angle double left"/>
       </button>
       <div
         className={styles.sidebarInner}
