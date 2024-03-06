6aa0dedae8697d1af2b32aa612575038ed2d23ef
Ralf Junghanns <ralf@junghanns.it>
2024-03-06 15:10:27 +0100

    Refactor global less files

--- src/frontend/src/components/Slider/SimpleSlider/index.ts
+++ src/frontend/src/components/Slider/SimpleSlider/index.ts
@@ -1,7 +1,6 @@
+import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './rc-slider.css';
-
-import Slider from 'rc-slider';
 
 export default Slider;
 export * from 'rc-slider';
