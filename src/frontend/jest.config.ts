72b59aeff8cb3e3daebc5f946f7de929b1c6ba06
Ralf Junghanns <ralf@junghanns.it>
2024-03-25 15:05:33 +0100

    Update Mock

--- src/frontend/jest.config.ts
+++ src/frontend/jest.config.ts
@@ -39,7 +39,7 @@ export default async (): Promise<Config.InitialOptions> => ({
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
     'react-i18next': '<rootDir>/__mocks__/reacti18nextMock.js',
-    'react-oidc-context': '<rootDir>/__mocks__/reactOidcContextMock.js',
+    'react-oidc-context': '<rootDir>/__mocks__/react_oidc_context_mock.js',
     "^swiper/react": "<rootDir>/node_modules/swiper/swiper-react.d.ts",
     "^swiper/modules": "<rootDir>/node_modules/swiper/types/modules/index.d.ts",
     "^(swiper/css|swiper/css/navigation|swiper/css/pagination)": '<rootDir>/__mocks__/styleMock.js',
