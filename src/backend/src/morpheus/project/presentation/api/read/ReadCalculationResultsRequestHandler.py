91cefcf597f3d9bbff4d00b8e04a1cf664ab1213
Ralf Junghanns <ralf@junghanns.it>
2024-06-30 22:00:16 +0200

    Merge main

--- src/backend/src/morpheus/project/presentation/api/read/ReadCalculationResultsRequestHandler.py
+++ src/backend/src/morpheus/project/presentation/api/read/ReadCalculationResultsRequestHandler.py
@@ -1,8 +1,7 @@
-from morpheus.common.types.Exceptions import NotFoundException, InsufficientPermissionsException
+import math
+from morpheus.common.types.Exceptions import InsufficientPermissionsException, NotFoundException
 from ....application.read.PermissionsReader import permissions_reader
 from ....incoming import get_identity
-import math
-from morpheus.common.types.Exceptions import NotFoundException
 from ....application.read.CalculationReader import get_calculation_reader
 from ....infrastructure.assets.RasterInterpolationService import RasterInterpolationService
 from ....infrastructure.calculation.engines.base.CalculationEngineFactory import CalculationEngineFactory
@@ -13,11 +12,6 @@ from ....types.discretization.spatial import Grid
 
 
 class ReadCalculationResultsRequestHandler:
-    def handle(self, project_id: ProjectId, calculation_id: CalculationId, result_type: str, idx: int, layer: int = 0, incremental: bool = False):
-        identity = get_identity()
-        if identity is None:
-            return '', 401
-
     def _interpolate_data(self, data: list[list[float | None]], grid: Grid):
         raster_interpolator = RasterInterpolationService()
         target_resolution_x = grid.n_cols() if grid.n_cols() < 200 else int(grid.n_cols() / 2)
@@ -45,98 +39,104 @@ class ReadCalculationResultsRequestHandler:
         }
 
     def handle(self, project_id: ProjectId, calculation_id: CalculationId, result_type: str, time_idx: int, layer_idx: int = 0, incremental: bool = False):
+        identity = get_identity()
+        if identity is None:
+            return '', 401
 
         try:
             permissions_reader.assert_identity_can(Privilege.VIEW_PROJECT, identity, project_id)
             available_result_types = ['flow_head', 'flow_drawdown', 'flow_budget', 'transport_concentration', 'transport_budget']
+
             if result_type not in available_result_types:
-                raise NotFoundException(f'Result type not found, available types are: {", ".join(available_result_types)}')
+                raise ValueError(f'Result type not found, available types are: {", ".join(available_result_types)}')
 
-        calculation_reader = get_calculation_reader()
-        calculation = calculation_reader.get_calculation(project_id=project_id, calculation_id=calculation_id)
+            calculation_reader = get_calculation_reader()
+            calculation = calculation_reader.get_calculation(project_id=project_id, calculation_id=calculation_id)
 
-        if calculation is None:
-            raise NotFoundException(f'Calculation {calculation_id.to_str()} not found in project {project_id.to_str()}')
+            if calculation is None:
+                raise NotFoundException(f'Calculation {calculation_id.to_str()} not found in project {project_id.to_str()}')
 
-        model = calculation_reader.get_model_by_calculation_id(project_id=project_id, calculation_id=calculation_id)
-        if model is None:
-            raise NotFoundException(f'Model for calculation {calculation_id.to_str()} not found in project {project_id.to_str()}')
+            model = calculation_reader.get_model_by_calculation_id(project_id=project_id, calculation_id=calculation_id)
+            if model is None:
+                raise NotFoundException(f'Model for calculation {calculation_id.to_str()} not found in project {project_id.to_str()}')
 
-        engine = CalculationEngineFactory.create_engine(calculation_id=calculation_id, engine_type=calculation.get_engine_type())
-        grid = model.spatial_discretization.grid
+            engine = CalculationEngineFactory.create_engine(calculation_id=calculation_id, engine_type=calculation.get_engine_type())
+            grid = model.spatial_discretization.grid
 
-        if result_type == 'flow_head':
-            data = engine.read_flow_head(idx=time_idx, layer=layer_idx, precision=4)
-            data = [[None if cell < -999 else cell for cell in row] for row in data]
+            if result_type == 'flow_head':
+                data = engine.read_flow_head(idx=time_idx, layer=layer_idx, precision=4)
+                data = [[None if cell < -999 else cell for cell in row] for row in data]
 
-            # interpolate data to irregular grid
-            if not grid.is_regular() or grid.n_cols() > 200:
-                data = self._interpolate_data(data, grid)
+                # interpolate data to irregular grid
+                if not grid.is_regular() or grid.n_cols() > 200:
+                    data = self._interpolate_data(data, grid)
 
-            result = {
-                'result_type': result_type,
-                'layer_idx': layer_idx,
-                'time_idx': time_idx,
-                'data': self._get_result_data_object(data=data, grid=grid)
-            }
+                result = {
+                    'result_type': result_type,
+                    'layer_idx': layer_idx,
+                    'time_idx': time_idx,
+                    'data': self._get_result_data_object(data=data, grid=grid)
+                }
 
-            return result, 200
+                return result, 200
 
-        if result_type == 'flow_drawdown':
-            data = engine.read_flow_drawdown(idx=time_idx, layer=layer_idx, precision=4)
-            data = [[None if cell < -999 else cell for cell in row] for row in data]
+            if result_type == 'flow_drawdown':
+                data = engine.read_flow_drawdown(idx=time_idx, layer=layer_idx, precision=4)
+                data = [[None if cell < -999 else cell for cell in row] for row in data]
 
-            # interpolate data to irregular grid
-            if not grid.is_regular():
-                data = self._interpolate_data(data, grid)
+                # interpolate data to irregular grid
+                if not grid.is_regular():
+                    data = self._interpolate_data(data, grid)
 
-            result = {
-                'result_type': result_type,
-                'layer_idx': layer_idx,
-                'time_idx': time_idx,
-                'data': self._get_result_data_object(data=data, grid=grid)
-            }
+                result = {
+                    'result_type': result_type,
+                    'layer_idx': layer_idx,
+                    'time_idx': time_idx,
+                    'data': self._get_result_data_object(data=data, grid=grid)
+                }
 
-            return result, 200
+                return result, 200
 
-        if result_type == 'transport_concentration':
-            data = engine.read_transport_concentration(idx=time_idx, layer=layer_idx, precision=4)
-            data = [[None if cell < -999 else cell for cell in row] for row in data]
+            if result_type == 'transport_concentration':
+                data = engine.read_transport_concentration(idx=time_idx, layer=layer_idx, precision=4)
+                data = [[None if cell < -999 else cell for cell in row] for row in data]
 
-            # interpolate data to irregular grid
-            if not grid.is_regular():
-                data = self._interpolate_data(data, grid)
+                # interpolate data to irregular grid
+                if not grid.is_regular():
+                    data = self._interpolate_data(data, grid)
 
-            result = {
-                'result_type': result_type,
-                'layer_idx': layer_idx,
-                'time_idx': time_idx,
-                'data': self._get_result_data_object(data=data, grid=grid)
-            }
+                result = {
+                    'result_type': result_type,
+                    'layer_idx': layer_idx,
+                    'time_idx': time_idx,
+                    'data': self._get_result_data_object(data=data, grid=grid)
+                }
 
-            return result, 200
+                return result, 200
 
-        if result_type == 'flow_budget':
-            data = engine.read_flow_budget(idx=time_idx, incremental=incremental)
-            result = {
-                'result_type': result_type,
-                'time_idx': time_idx,
-                'data': data,
-                'incremental': incremental
-            }
-            return result, 200
+            if result_type == 'flow_budget':
+                data = engine.read_flow_budget(idx=time_idx, incremental=incremental)
+                result = {
+                    'result_type': result_type,
+                    'time_idx': time_idx,
+                    'data': data,
+                    'incremental': incremental
+                }
+                return result, 200
 
-        if result_type == 'transport_budget':
-            data = engine.read_transport_budget(idx=time_idx, incremental=incremental)
-            result = {
-                'result_type': result_type,
-                'time_idx': time_idx,
-                'data': data,
-                'incremental': incremental
-            }
-            return result, 200
+            if result_type == 'transport_budget':
+                data = engine.read_transport_budget(idx=time_idx, incremental=incremental)
+                result = {
+                    'result_type': result_type,
+                    'time_idx': time_idx,
+                    'data': data,
+                    'incremental': incremental
+                }
+                return result, 200
 
-            raise NotFoundException(f'Result type not found, available types are: {", ".join(available_result_types)}')
+            raise ValueError(f'Result type not found, available types are: {", ".join(available_result_types)}')
+        except ValueError as e:
+            return str(e), 400
         except InsufficientPermissionsException as e:
             return str(e), 403
         except NotFoundException as e:
