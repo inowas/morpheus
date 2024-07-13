43d25b7908fea77df92ed6c9ddc6f33ad51a7841
Ralf Junghanns <ralf@junghanns.it>
2024-07-13 16:59:39 +0200

    Add Pie-Charts to Budget Results

--- src/frontend/src/morpheus/modflow/presentation/components/Results/BudgetSectionChart.tsx
+++ src/frontend/src/morpheus/modflow/presentation/components/Results/BudgetSectionChart.tsx
@@ -2,8 +2,8 @@ import React, {useMemo} from 'react';
 import {DataGrid, Form, Label} from 'common/components';
 import {CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'common/components/RechartsWrapper';
 import {IBudgetData} from '../../../application/useCalculationResults';
-import {Bar, BarChart, Cell} from 'recharts';
-import {Checkbox, List, Segment} from 'semantic-ui-react';
+import {Bar, BarChart, Cell, Pie, PieChart} from 'recharts';
+import {Checkbox, Header, List, Segment, Tab, TabPane} from 'semantic-ui-react';
 
 
 interface IProps {
@@ -21,7 +21,14 @@ type IBarChartData = {
   color: string;
 }[];
 
-const isValidBudgetBarChartKey = (key: string) => {
+type IPieChartData = {
+  name: string;
+  value: number;
+  color: string;
+}[];
+
+
+const isValidBudgetChartKey = (key: string) => {
   if ('PERCENT_DISCREPANCY' === key) {
     return false;
   }
@@ -33,7 +40,7 @@ const BudgetChart = ({data, isIncremental, onChangeIsIncremental, colors}: IProp
   const [disabledItems, setDisabledItems] = React.useState<string[]>([]);
 
   const barChartData: IBarChartData = useMemo(() => {
-    const dataKeys = Object.keys(data.data).filter((key) => isValidBudgetBarChartKey(key));
+    const dataKeys = Object.keys(data.data).filter((key) => isValidBudgetChartKey(key));
     dataKeys.sort();
     return dataKeys.map((key, idx) => ({
       name: key,
@@ -44,6 +51,29 @@ const BudgetChart = ({data, isIncremental, onChangeIsIncremental, colors}: IProp
     }));
   }, [data.data, disabledItems]);
 
+
+  const pieChartData: [IPieChartData, IPieChartData] = useMemo(() => {
+    const dataKeys = Object.keys(data.data).filter((key) => isValidBudgetChartKey(key));
+    const inFlowKeys = dataKeys.filter((key) => key.endsWith('_IN') && !key.startsWith('TOTAL'));
+    const outFlowKeys = dataKeys.filter((key) => key.endsWith('_OUT') && !key.startsWith('TOTAL'));
+
+    const inFlowData = inFlowKeys.map((key, idx) => ({
+      name: key,
+      value: data.data[key],
+      color: colors[idx % colors.length],
+    }));
+
+    const outFlowData = outFlowKeys.map((key, idx) => ({
+      name: key,
+      value: data.data[key] * -1,
+      color: colors[idx % colors.length],
+    }));
+
+    return [inFlowData, outFlowData];
+
+
+  }, [data.data]);
+
   const percentDiscrepancy: number | null = useMemo(() => {
     if (!('PERCENT_DISCREPANCY' in data.data)) {
       return null;
@@ -52,27 +82,13 @@ const BudgetChart = ({data, isIncremental, onChangeIsIncremental, colors}: IProp
     return data.data.PERCENT_DISCREPANCY;
   }, [data.data]);
 
-
-  return (
-    <DataGrid>
-      <Form.Form style={{margin: 10}}>
-        <Form.Group>
-          <Label>Type</Label>
-          <Form.Radio
-            label={'Incremental'}
-            checked={isIncremental}
-            onChange={(e, {checked}) => onChangeIsIncremental(!!checked)}
-          />
-          <Form.Radio
-            label={'Cumulative'}
-            checked={!isIncremental}
-            onChange={(e, {checked}) => onChangeIsIncremental(!checked)}
-          />
-        </Form.Group>
-      </Form.Form>
-
+  const renderBarChart = () => (
+    <>
       <ResponsiveContainer aspect={16 / 9}>
-        <BarChart data={barChartData.filter(item => !item.isDisabled)}>
+        <BarChart
+          data={barChartData.filter(item => !item.isDisabled)}
+          margin={{top: 10, right: 10, left: -10, bottom: 10}}
+        >
           <CartesianGrid strokeDasharray='3 3'/>
           <XAxis
             dataKey='name'
@@ -92,29 +108,101 @@ const BudgetChart = ({data, isIncremental, onChangeIsIncremental, colors}: IProp
 
       {null !== percentDiscrepancy && <span style={{textAlign: 'right'}}>{null != percentDiscrepancy && <p>Discrepancy: {percentDiscrepancy.toFixed(2)}%</p>}</span>}
 
-      <Segment color={'grey'}>
-        <List>
-          {barChartData.map((item) => (
-            <List.Item key={item.name}>
-              <List.Content>
-                <Form.Group>
-                  <Checkbox
-                    checked={!item.isDisabled}
-                    label={item.name}
-                    onChange={() => {
-                      if (item.isDisabled) {
-                        return setDisabledItems(disabledItems.filter((key) => key !== item.name));
-                      }
-                      setDisabledItems([...disabledItems, item.name]);
-                    }}
-                  />
-                </Form.Group>
-              </List.Content>
-            </List.Item>
-          ))}
-        </List>
-      </Segment>
+      <List>
+        {barChartData.map((item) => (
+          <List.Item key={item.name}>
+            <List.Content>
+              <Form.Group>
+                <Checkbox
+                  checked={!item.isDisabled}
+                  label={item.name}
+                  onChange={() => {
+                    if (item.isDisabled) {
+                      return setDisabledItems(disabledItems.filter((key) => key !== item.name));
+                    }
+                    setDisabledItems([...disabledItems, item.name]);
+                  }}
+                />
+              </Form.Group>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    </>
+  );
 
+  const renderPieChart = () => (
+    <>
+      <Header as={'h3'}>Inflow</Header>
+      <ResponsiveContainer aspect={16 / 9}>
+        <PieChart
+          margin={{top: 10, right: 10, left: -10, bottom: 10}}
+          key={'inflow'}
+          title={'Inflow'}
+        >
+          <Pie
+            data={pieChartData[0]}
+            dataKey='value'
+            nameKey='name'
+            cx='50%'
+            cy='50%'
+            outerRadius={100}
+            fill='#8884d8'
+            label={({name, value}) => `${name} (${value.toExponential()})`}
+          >
+            {pieChartData[0].map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color}/>
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <Header as={'h3'}>Outflow</Header>
+      <ResponsiveContainer aspect={16 / 9}>
+        <PieChart
+          margin={{top: 10, right: 10, left: -10, bottom: 10}}
+          key={'outflow'}
+          title={'Outflow'}
+        >
+          <Pie
+            data={pieChartData[1]}
+            dataKey='value'
+            nameKey='name'
+            cx='50%'
+            cy='50%'
+            outerRadius={100}
+            fill='#8884d8'
+            label={({name, value}) => `${name} (${value.toExponential()})`}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </>
+  );
+
+
+  return (
+    <DataGrid>
+      <Form.Form style={{margin: 10, marginBottom: 0}}>
+        <Form.Group>
+          <Label>Type</Label>
+          <Form.Radio
+            label={'Incremental'}
+            checked={isIncremental}
+            onChange={(e, {checked}) => onChangeIsIncremental(!!checked)}
+          />
+          <Form.Radio
+            label={'Cumulative'}
+            checked={!isIncremental}
+            onChange={(e, {checked}) => onChangeIsIncremental(!checked)}
+          />
+        </Form.Group>
+      </Form.Form>
+
+      <Tab
+        panes={[
+          {menuItem: 'Bars', render: () => <TabPane attached={true}>{renderBarChart()}</TabPane>},
+          {menuItem: 'Pies', render: () => <TabPane attached={true}>{renderPieChart()}</TabPane>},
+        ]}
+      />
     </DataGrid>
   );
 };
