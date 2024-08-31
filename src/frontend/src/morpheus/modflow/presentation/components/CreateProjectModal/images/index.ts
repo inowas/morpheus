7641f802aeb8b70450ccf1dc96c4f8cf5c3a0a7a
Catalin Stefan <catalin.stefan@tu-dresden.de>
2024-09-01 01:47:39 +0200

    Styling 'Create new project' modal

--- src/frontend/src/morpheus/modflow/presentation/components/CreateProjectModal/images/index.ts
+++ src/frontend/src/morpheus/modflow/presentation/components/CreateProjectModal/images/index.ts
@@ -1,20 +1,10 @@
 const Images: string[] = [
-  require('./create-new-project.svg'),
-  require('./create-new-project-2.svg'),
-  require('./create-new-project-3.svg'),
-  require('./create-new-project-4.svg'),
-  require('./create-new-project-5.svg'),
-  require('./create-new-project-6.svg'),
-  require('./create-new-project-7.svg'),
-  require('./create-new-project-8.svg'),
-  require('./create-new-project-9.svg'),
-  require('./create-new-project-10.svg'),
-  require('./create-new-project-11.svg'),
-  require('./create-new-project-12.svg'),
-  require('./create-new-project-13.svg'),
-  require('./create-new-project-14.svg'),
-  require('./create-new-project-15.svg'),
-  require('./create-new-project-16.svg'),
+  require('./new-1.jpg'),
+  require('./new-2.jpg'),
+  require('./new-3.jpg'),
+  require('./new-4.jpg'),
+  require('./new-5.jpg'),
+  require('./new-6.jpg'),
 ];
 
 export default Images;
