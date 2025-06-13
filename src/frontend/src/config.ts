37d83e9c97c662ec589553076f1e630fa13ab802
Ralf Junghanns <ralf.junghanns@gmail.com>
2025-06-13 07:47:59 +0200

    Update config for v0.13

--- src/frontend/src/config.ts
+++ src/frontend/src/config.ts
@@ -1,14 +1,29 @@
 import * as process from 'process';
 
+const getIdentityServerUrl = () => {
+  if (process.env.IDENTITY_SERVER_URL) {
+    return process.env.IDENTITY_SERVER_URL;
+  }
+
+  const hostname = window.location.hostname;
+  // Legacy support for Morpheus
+  if (hostname.includes('morpheus.inowas.com')) {
+    return 'https://identity.inowas.com';
+  }
+
+  return `//identity.${hostname}`;
+};
+
+
 const config = {
-  baseApiUrl: process.env.BASE_API_URL || 'http://localhost:4000/api/v1',
+  baseApiUrl: process.env.BASE_API_URL || `//api.${window.location.hostname}`,
   modflowApiUrl: process.env.MODFLOW_API_URL || 'https://modflow.inowas.com',
   release: process.env.GIT_RELEASE || 'dev',
   releaseDate: process.env.GIT_RELEASE_DATE || 'unknown',
   keycloak: {
-    url: process.env.KEYCLOAK_URL || 'https://identity.inowas.com',
-    realm: process.env.KEYCLOAK_REALM || 'inowas-dev',
-    clientId: process.env.KEYCLOAK_CLIENT_ID || 'morpheus-frontend',
+    url: getIdentityServerUrl(),
+    realm: process.env.KEYCLOAK_REALM || 'inowas',
+    clientId: process.env.KEYCLOAK_CLIENT_ID || 'inowas-frontend',
   },
   debug: window.location.search.includes('debug=true') || 'true' === process.env.DEBUG || false,
   sentry: {
